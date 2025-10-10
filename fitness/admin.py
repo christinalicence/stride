ebb59e691ce3771ed7b03120aa5740b1641655b6
christinalicence <christinalicence@gmail.com>
2025-10-10 17:29:45 +0100

    update admin.py with models, adding summernote for some fields

--- fitness/admin.py
+++ fitness/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import UserProfile, TrainingPlan, Comment
 
 # Register your models here.
+# Summernote on;y for some fields that don't go to LLM
+
+@admin.register(UserProfile)
+class UserProfileAdmin(SummernoteModelAdmin):
+    # Enable Summernote for the UserProfile bio field
+    summernote_fields = ('bio',)
+
+@admin.register(TrainingPlan)
+class TrainingPlanAdmin(SummernoteModelAdmin):
+    # Enable Summernote for plan descriptions and user progress comments
+    summernote_fields = ('plan_summary', 'progress_comment',)
+
+@admin.register(Comment)
+class CommentAdmin(SummernoteModelAdmin):
+    # Enable Summernote for comment content
+    summernote_fields = ('content',)
