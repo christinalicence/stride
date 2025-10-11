4c244e46dcc6a6ccd50faff2f561cb14e7c5edbf
christinalicence <christinalicence@gmail.com>
2025-10-11 14:51:04 +0100

    hook up to a fresh database, create a superuser

--- fitness/views.py
+++ fitness/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import UserProfile, TrainingPlan, Comment
 
 # Create your views here.
