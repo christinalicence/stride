5a60837e65826a4bb87743d7e1ca305f3674192e
christinalicence <christinalicence@gmail.com>
2025-10-13 17:06:56 +0100

    fix links in profile_detail so comments show properly

--- fitness/urls.py
+++ fitness/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('profiles/', views.profile_list, name='profile_list'),
     path('profiles/<str:username>/', views.profile_detail, name='profile_detail'),
     path('profiles/edit/', views.edit_profile, name='edit_profile'),
-    path('profiles/<str:username>/comment/', views.add_comment, name='add_comment'),
+    path('profiles/<int:profile_pk>/comment/', views.add_comment, name='add_comment'),
     path('comments/<int:comment_id>/edit/', views.edit_comment, name='edit_comment'),
     path('comments/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
 ]
