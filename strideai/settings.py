272ee6d2a31ef2224c2110931ecbbf360d2cc07b
christinalicence <christinalicence@gmail.com>
2025-10-13 10:03:10 +0100

    update allowed hosts

--- strideai/settings.py
+++ strideai/settings.py
@@ -28,8 +28,11 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '.herokuapp.com',
+]
 
 # Application definition
 
