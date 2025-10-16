e5f488ad9602cf30bf8e8942c75aaffc218e0279
christinalicence <christinalicence@gmail.com>
2025-10-16 11:06:13 +0100

    change db url to config instead of parse to help 500 errors

--- strideai/settings.py
+++ strideai/settings.py
@@ -90,7 +90,11 @@ DATABASE_URL = os.environ.get('DATABASE_URL')
 
 if DATABASE_URL:
     DATABASES = {
-        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True,
+        )
     }
 else:
     DATABASES = {
@@ -101,7 +105,7 @@ else:
     }
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://*.herokuapp.com"
+    "https://*.herokuapp.com,"
     "https://strideai-3c07c7acd94b.herokuapp.com"
 ]
 
