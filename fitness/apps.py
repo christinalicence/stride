a15a9be6588fd01a640ce3fa8859e8c3ea8c5813
christinalicence <christinalicence@gmail.com>
2025-11-13 15:59:14 +0000

    fix issue with signals and profile creation

--- fitness/apps.py
+++ fitness/apps.py
@@ -5,8 +5,7 @@ class FitnessConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "fitness"
 
-
-# imports signals so profiles are created/updated automatically
-def ready(self):
-    import fitness.signals
-    _ = fitness.signals
+    # imports signals so profiles are created/updated automatically
+    def ready(self):
+        import fitness.signals
+        _ = fitness.signals
